48ceb0dd1fa4b64ab794e399ca6f98594b1e2da0
ajay bansode <ajaybansode1234@gmail.com>
2025-08-07 19:23:29 +0530

    Add project files.

---
+++ WebApplicationCalenderControl/WebFormCalender1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormCalender1.aspx.cs" Inherits="WebApplicationCalenderControl.WebFormCalender1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Calender control Display Message</h2>
+            <asp:Calendar ID="Calendar1" runat="server" 
+                OnSelectionChanged="Calendar1_SelectionChanged"
+                OnDayRender="Calendar1_DayRender"></asp:Calendar>
+            <br />
+            <asp:Label ID="lblMessage" runat="server" Font-Bold="true" Font-Size="Larger" ForeColor="SteelBlue"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplicationCalenderControl/WebFormCalender1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationCalenderControl
+{
+    public partial class WebFormCalender1:System.Web.UI.Page
+    {
+        protected void Page_Load(object sender,EventArgs e)
+        {
+
+        }
+
+        public static readonly Dictionary<DateTime,string> impDates = new Dictionary<DateTime,string>() {
+            {new DateTime(2025,8,15),"Independence Day" },
+            {new DateTime(2025,8,9),"RakshaBandhan" },
+            {new DateTime(2025,8,19),"Janmashtami" },
+            {new DateTime(2025,8,27),"Ganesh Chaturthi" },
+                    };
+
+        protected void Calendar1_SelectionChanged(object sender,EventArgs e)
+        {
+            DateTime selectedDate = Calendar1.SelectedDate;
+            lblMessage.Text = "Your Selected: " + selectedDate.ToString("dd/MM/yyyy");
+        }
+
+        protected void Calendar1_DayRender(object sender,DayRenderEventArgs e)
+        {
+            if(impDates.ContainsKey(e.Day.Date))
+            {
+                //e.Cell.BackColor = System.Drawing.Color.RoyalBlue;
+                e.Cell.Controls.Add(new LiteralControl("<br/> <span style='color:red;'>" + impDates[e.Day.Date] + "</span>"));
+            }
+        }
+    }
+}
